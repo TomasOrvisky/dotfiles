bb4260d61f1554334ac41dbe419bc67d28cd09c0
ortom <tomasorvisky62@gmail.com>
2026-04-08 22:16:33 +0200

    init

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+sudo apt update
+sudo apt install -y \
+    xorg \
+    i3 i3status picom \
+    kitty fish \
+    stow \
+    feh
+
+stow i3 i3status picom fish
+
+chsh -s $(which fish)
